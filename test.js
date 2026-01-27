32067e874626dbd1963aae58a5af9f8049a0c21d
Rod Vagg <rod@vagg.org>
2026-01-27 22:06:13 +1100

    chore!: modernise, ESM, promises, update deps, GHA, auto-release

--- test.js
+++ test.js
@@ -1,25 +1,35 @@
-const ghutils = require('ghutils/test')
-    , http    = require('http')
-    , test    = require('tape')
-    , xtend   = require('xtend')
-    , ghusers = require('./')
+import { test } from 'node:test'
+import assert from 'node:assert'
+import { createMockServer } from 'ghutils/test-util'
+import * as ghusers from './ghusers.js'
 
+test('get user', async () => {
+  const auth = { token: 'test-token' }
+  const testData = { login: 'testuser', id: 1, name: 'Test User' }
 
-test('test get user', function (t) {
-  t.plan(7)
-
-  var auth     = { user: 'authuser', token: 'authtoken' }
-    , username = 'testuser'
-    , testData = { login: username, test: 'data' }
-    , server
-
-  server = ghutils.makeServer(testData)
-    .on('ready', function () {
-      ghusers.get(xtend(auth), username, ghutils.verifyData(t, testData))
+  const server = await createMockServer({ response: testData })
+  try {
+    const result = await ghusers.get(auth, 'testuser', {
+      _apiUrl: server.baseUrl
     })
-    .on('request', ghutils.verifyRequest(t, auth))
-    .on('get', ghutils.verifyUrl(t, [
-        'https://api.github.com/users/' + username
-    ]))
-    .on('close'  , ghutils.verifyClose(t))
+    assert.deepStrictEqual(result, testData)
+    assert.ok(server.requests[0].url.includes('/users/testuser'))
+    assert.strictEqual(server.requests[0].headers.authorization, 'Bearer test-token')
+  } finally {
+    await server.close()
+  }
+})
+
+test('auth header is correctly set', async () => {
+  const auth = { token: 'my-secret-token' }
+  const testData = { login: 'testuser' }
+
+  const server = await createMockServer({ response: testData })
+  try {
+    await ghusers.get(auth, 'testuser', { _apiUrl: server.baseUrl })
+    assert.strictEqual(server.requests[0].headers.authorization, 'Bearer my-secret-token')
+    assert.strictEqual(server.requests[0].headers.accept, 'application/vnd.github+json')
+  } finally {
+    await server.close()
+  }
 })
